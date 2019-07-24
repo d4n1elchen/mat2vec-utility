7cf5c29f42d0d7e79084c2779c9d12288fc1187a
Daniel Chen <team6612@gmail.com>
2019-07-24 14:56:31 +0900

    Add tqdm progress bar for batch process

--- batch_process.py
+++ batch_process.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from tqdm import tqdm
 from mat2vec.processing import MaterialsTextProcessor
 
 text_processor = MaterialsTextProcessor()
@@ -11,9 +12,15 @@ parser.add_argument('--save_path', type=str, default='corpus.txt', help='Save pa
 
 args = parser.parse_args()
 
+def get_num_lines(file_path):
+    with open(file_path) as f:
+        n = sum(1 for l in f)
+    return n
+
+print('Read text form', args.source_path, ', write processed sentence to', args.save_path)
 with open(args.source_path) as infile:
     with open(args.save_path, 'w+') as outfile:
-        for line in infile:
+        for line in tqdm(infile, total=get_num_lines(args.source_path)):
             words, _ = text_processor.process(line)
             outfile.write(' '.join(words) + '\n')
 
